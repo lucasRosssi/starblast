e32b848ad9cfbef56127cc1a2b7161ad31b7d8fd
Lucas Rossi <lucasrm2305@gmail.com>
2025-06-12 21:08:36 -0300

    create interfaces

---
+++ Source/Starblast/Private/Interfaces/AttributeSetInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/AttributeSetInterface.h"
+
+// Add default functionality here for any IAttributeSetInterface functions that are not pure virtual.
